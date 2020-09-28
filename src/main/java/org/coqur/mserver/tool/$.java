90cfea79d7444d997e5ff221006372e45efbd5b7
aizichen <1098679804@qq.com>
2020-09-29 07:20:51 +0800

    add slf4j

---
+++ src/main/java/org/coqur/mserver/tool/$.java
@@ -0,0 +1,55 @@
+package org.coqur.mserver.tool;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Tools
+ *
+ * @author Quanyec
+ */
+public class $ {
+    private final static int HEX_BASE_SIZE = 16;
+
+    public static $ newInstance() {
+        return new $();
+    }
+
+    public long hexToLong(@NotNull String hex) {
+        int len = hex.length();
+        if (len == 0) {
+            return 0;
+        }
+        long total = 0;
+        for (int i = 0; i < len; ++i) {
+            char c = hex.charAt(i);
+            if (c >= '0' && c <= '9') {
+                int n = c - '0';
+                total = total + n * (long) Math.pow(HEX_BASE_SIZE, len - 1 - i);
+            } else if (c >= 'a' && c <= 'f') {
+                int n = 10 + c - 'a';
+                total = total + n * (long) Math.pow(HEX_BASE_SIZE, len - 1 - i);
+            } else if (c >= 'A' && c <= 'F') {
+                int n = 10 + c - 'A';
+                total = total + n * (long) Math.pow(HEX_BASE_SIZE, len - 1 - i);
+            }
+        }
+
+        return total;
+    }
+
+
+    public String readLine(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int b;
+        while ((b = is.read()) != -1) {
+            sb.append((char) b);
+            if (b == '\n') {
+                return sb.toString();
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/org/coqur/mserver/tool/Net$.java
@@ -0,0 +1,69 @@
+package org.coqur.mserver.tool;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Net$ {
+
+    private final $ tools;
+
+    private Net$() {
+        this.tools = $.newInstance();
+    }
+
+    public static Net$ newInstance() {
+        return new Net$();
+    }
+
+    public void writeExactlySize(InputStream dis, OutputStream cos, long exactlySize) throws IOException {
+        int totalRead = 0;
+        int hasRead;
+        byte[] buff = new byte[2048];
+        while ((hasRead = dis.read(buff)) != -1) {
+            cos.write(buff, 0, hasRead);
+            totalRead += hasRead;
+            if (totalRead >= exactlySize) {
+                break;
+            }
+        }
+    }
+
+    public void writeChunked(InputStream dis, OutputStream cos) throws IOException {
+        // `transfer-encoding` value is chunked
+        while (true) {
+            String chunkedLenStr = tools.readLine(dis);
+            if (chunkedLenStr == null) {
+                break;
+            }
+            cos.write(chunkedLenStr.getBytes());
+
+            chunkedLenStr = chunkedLenStr.trim();
+            if (chunkedLenStr.equals("0")) {
+//                System.out.println("chunked code is now zero.");
+                cos.write("\r\n".getBytes());
+                break;
+            }
+            long chunkedLen = tools.hexToLong(chunkedLenStr);
+            int b;
+            for (int totalRead = 0; (b = dis.read()) != -1; totalRead++) {
+                cos.write(b);
+                if (totalRead >= chunkedLen + 1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public String readIsHeader(InputStream is) throws IOException {
+        StringBuilder hsb = new StringBuilder();
+        String line;
+        while ((line = tools.readLine(is)) != null) {
+            hsb.append(line);
+            if (line.isBlank()) {
+                break;
+            }
+        }
+        return hsb.toString();
+    }
+}
