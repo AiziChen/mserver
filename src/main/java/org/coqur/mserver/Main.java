9bf1cc1c9f79608635dcaaedf072940b93b0d9d1
aizi <1098679804@qq.com>
2020-09-29 09:59:19 +0800

    Change the `java.util.logging` logger to `slf4j` logger

--- src/main/java/org/coqur/mserver/Main.java
+++ src/main/java/org/coqur/mserver/Main.java
@@ -1,9 +1,10 @@
 package org.coqur.mserver;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static final Logger LOGGER = Logger.getGlobal();
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         new Server(args);
